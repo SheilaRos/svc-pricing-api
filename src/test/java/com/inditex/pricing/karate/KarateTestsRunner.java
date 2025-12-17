d425e1c328c4aa1c95c2f6fcde51ecc23c21ab67
sheila <sheila.ros.cheme@gmail.com>
2025-12-17 22:40:53 +0100

    fix karate db

--- src/test/java/com/inditex/pricing/karate/KarateTestsRunner.java
+++ src/test/java/com/inditex/pricing/karate/KarateTestsRunner.java
@@ -2,12 +2,16 @@ package com.inditex.pricing.karate;
 
 import com.inditex.pricing.PricingApplication;
 import com.intuit.karate.junit5.Karate;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(classes = {PricingApplication.class},
-        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
+        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+        properties = {
+                "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE"
+        }
 )
 @ActiveProfiles({"test"})
 @TestPropertySource(properties = "server.port=8082")
